2bfa209e3925a6916f206f7d0faa8af6d2e729d1
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-10-04 17:23:04 +0900

    Update create.tsx

--- components/create.tsx
+++ components/create.tsx
@@ -7,7 +7,6 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Checkbox } from "@/components/ui/checkbox"
 import {
