529e7e015a93248ad73c809114e1e80ebf05d74a
kawajiri <jiri0521@yahoo.co.jp>
2024-10-05 11:41:06 +0900

    resolve api error

--- app/api/post/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prismaClient";
-
-export async function GET(){
-const AllReport = await prisma.incident.findMany();
-return NextResponse.json(AllReport);
-
-}
