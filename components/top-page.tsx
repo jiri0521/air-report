8e51b9cb89336567143c2bd9e5332547b690b0a0
kawajiri <jiri0521@yahoo.co.jp>
2024-10-08 00:17:58 +0900

    dark mode

--- components/top-page.tsx
+++ components/top-page.tsx
@@ -72,8 +72,8 @@ export function TopPage() {
       </div>
     </main>
    
-    <footer className="bg-white shadow mt-8">
-      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+    <footer className="bg-white shadow mt-8 dark:bg-gray-800 text-white">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 dark:bg-gray-800 text-white">
         <p className="text-center text-sm text-gray-500 dark:bg-gray-800 text-white">
           © 2024 医療安全インシデントレポートシステム. All rights reserved.
         </p>
