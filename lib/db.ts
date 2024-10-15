5af4f9727549e95d8584b3c7b3d8b1023d1ecd71
kawajiri <jiri0521@yahoo.co.jp>
2024-10-16 00:59:25 +0900

    RESOLVE db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production" ) globalThis.prisma = db
+export const prisma = globalForPrisma.prisma || new PrismaClient();
