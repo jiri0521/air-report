85efe24c87982bdbf5e0b2511274c02659e82222
kawajiri <jiri0521@yahoo.co.jp>
2024-10-29 09:10:57 +0900

    edit suspense

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,13 +1,10 @@
 import CreatePage from '@/components/create'
 import React, { Suspense } from 'react'
-import Loading from '../Loading'
 
 const create = () => {
   return (
     <div>
-      <Suspense fallback={<Loading/>}>
       <CreatePage />
-      </Suspense>
     </div>
   )
 }
--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -2,14 +2,12 @@
 
 import ReportListPage from '@/components/report-list'
 import React, { Suspense } from 'react'
-import Loading from '../Loading'
+
 
 function ReportsPage() {
   return (
     <div>
-      <Suspense fallback={<Loading/>}>
       <ReportListPage />
-      </Suspense>
     </div>
   )
 }
