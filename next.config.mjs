4794d6b9a7ba98479ec14d2818e4bdcede48d272
kawajiri <jiri0521@yahoo.co.jp>
2024-11-02 11:59:28 +0900

    EDIT config

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,7 @@ const nextConfig = {
     images: {
       domains: ['res.cloudinary.com'],
     },
-    async headers() {
-        return [
-          {
-            source: '/:path*',
-            headers: [
-              {
-                key: 'Content-Security-Policy',
-                value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self'; connect-src 'self' https://api.resend.com",
-              },
-            ],
-          },
-        ]
-      },
+    
  
     
 };
