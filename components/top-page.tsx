4e2b85eb3fcfe2e1bca9a57e67b99c42c11363c0
kawajiri <jiri0521@yahoo.co.jp>
2025-03-24 13:34:51 +0900

    edit reoirt-list

--- components/top-page.tsx
+++ components/top-page.tsx
@@ -128,11 +128,6 @@ export function TopPage() {
     window.location.reload() // Force a full browser refresh
   }
 
-  useEffect(() => {
-    fetchData()
-   handleReload
-  }, [fetchData])
-
   const formatDate = (dateString: string) => {
     const date = new Date(dateString)
     return `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`
